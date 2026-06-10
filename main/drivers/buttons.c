bbc6ab2b097424b48e7b3595bf0321dfca8be6cd
rhammock1 <robert.hammock@gotab.io>
2026-06-10 12:27:37 -0400

    wip button label enum and create helper functions

--- main/drivers/buttons.c
+++ main/drivers/buttons.c
@@ -1,9 +1,30 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
 #include "buttons.h"
 #include "log.h"
 
 static const char *TAG = "BUTTON";
+
+#define DEBOUNCE_TIME_MS 50
+
+static const char *CAT_NAMES[] = {
+   [CAT_NONE] = "None",
+   [CAT_KITTY] = "Kitty",
+   [CAT_TODD] = "Todd",
+   [CAT_LADY] = "Lady",
+   [CAT_ROXY] = "Roxy",
+};
+
+// state variables
+static cat_label_t selected_cat = CAT_NONE;
+static bool recording = false;
+// last press times for debouncing
+static int64_t last_press_time[5] = {0}; // 4 cats + record button
 
 esp_err_t init_buttons(void)
 {
@@ -13,3 +34,23 @@ esp_err_t init_buttons(void)
     return ESP_OK;
 }
 
+cat_label_t get_selected_cat(void)
+{
+   if (selected_cat < CAT_NONE || selected_cat > CAT_ROXY) {
+      return CAT_NONE;
+   }
+    return selected_cat;
+}
+
+bool is_recording(void)
+{
+    return recording;
+}
+
+const char *cat_label_to_string(cat_label_t label)
+{
+   if (label < CAT_NONE || label > CAT_ROXY) {
+      return "Unknown";
+   }
+    return CAT_NAMES[label];
+}
--- main/include/buttons.h
+++ main/include/buttons.h
@@ -1,3 +1,6 @@
+#pragma once
+
+#include <stdbool.h>
 #include "esp_err.h"
 
 // Cat label button pins
@@ -9,5 +12,20 @@
 // Start/Stop Recording button pin
 #define RECORD_BUTTON_PIN 14
 
+// Which cat is currently selected for labeling
+typedef enum {
+    CAT_NONE = 0,
+    CAT_KITTY,
+    CAT_TODD,
+    CAT_LADY,
+    CAT_ROXY
+} cat_label_t;
+
 esp_err_t init_buttons(void);
+
+cat_label_t get_selected_cat(void);
+
+bool is_recording(void);
+
+const char *cat_label_to_string(cat_label_t label);
 
